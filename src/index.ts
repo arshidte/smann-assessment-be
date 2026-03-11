951f8bd280600c6687625998c074e88d1babdf46
ARSHID DIYAN TE <94746882+arshidte@users.noreply.github.com>
2026-03-11 23:29:06 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || 'https://smann-assessment.netlify.app/';
 
-app.use(cors({ origin: FRONTEND_ORIGIN }));
+// app.use(cors({ origin: FRONTEND_ORIGIN }));
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
